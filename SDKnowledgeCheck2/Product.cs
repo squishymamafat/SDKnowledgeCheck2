19dd970493652594f8e6634447171391b0ddc9c2
Josh Norman <135571751+squishymamafat@users.noreply.github.com>
2023-09-20 10:43:19 -0400

    Add files via upload

---
+++ SDKnowledgeCheck2/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDKnowledgeCheck2
+{
+    internal class Product
+    {
+    }
+}
+class CatFood : Product  // derived class (child)
+{
---
+++ SDKnowledgeCheck2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<MyClass>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var Product = new MyClass();
+
+    Console.WriteLine("Enter the value for ");
+    CatFood = Console.ReadLine();
+
+    recordList.Add(Product);
+}
